4097814191888d983bdfebb0ee2dc60ae704abea
aarisinger <avoyles23@ivytech.edu>
2025-10-12 23:27:52 -0500

    Module 7 changes

--- djangogirls/mysite/urls.py
+++ djangogirls/mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
